321d9f0393141fa7533cf2c84ef5aaaffe57d682
Mehdi <elmahdi20erraji@gmail.com>
2023-05-03 22:46:36 +0100

    main.js file created

---
+++ learn_front_end/30-days-of-JS/01_Day_Introduction/main.js
@@ -0,0 +1,5 @@
+// the variable value can be accessed from other variable.js file 
+
+console.log(firstname, lastname , country, city, age , isMarried)
+console.log(gravity, boilingpoint, PI) //9.81,100,3.14
+console.log(name ,job, live )
