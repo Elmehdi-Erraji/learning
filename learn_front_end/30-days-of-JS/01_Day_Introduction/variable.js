7eb397837802148898e22243dbed878624e25893
Mehdi <elmahdi20erraji@gmail.com>
2023-05-03 22:38:21 +0100

    variables

---
+++ learn_front_end/30-days-of-JS/01_Day_Introduction/variable.js
@@ -0,0 +1,22 @@
+// Declaring diffrent variables of diffrent data types 
+
+let firstname = 'Mehdi' //first name of a person
+let lastname = 'Raji' //last name of a person
+let country = 'Morocco' //country
+let city = "Agadir" //city 
+let age = 100 //age in 100years
+let isMarried = false
+
+//Declaring variables with number vlaue 
+
+const gravity = 9.81 //earth greavity
+const boilingpoint =100 //water boiling point, temperature 
+const PI = 3.14
+
+//Variables can also be declared in one line separated by a comma 
+
+let name = "mehdi", //name of a person
+    job = "student"
+    live = "morocco"
+
+console.log(firstname + ' '+ lastname +' i live in '+ country)
