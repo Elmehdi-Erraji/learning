cf2974cb15dbd4ef0796472dd0462954c637b6ee
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 01:07:43 +0100

    number data types

---
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/number_data_types.js
@@ -0,0 +1,9 @@
+let age = 35
+const gravity = 9.81 //we use const for non-changing values, gravitational constant in m/s2
+let mass = 72  //mass in kilogram
+const pi = 3.14 //pi a grometrical constant
+
+//more examples 
+const boilingoint = 100 // temoerature in 0c
+const bodytemp = 37
+console.log(age, gravity , mass, pi , boilingoint , bodytemp)
