d2cfda1d77480abbf129b942e9414bb7a2189d53
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 06:01:57 +0100

    char ascii code of a given index

---
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/char_code_at.js
@@ -0,0 +1,9 @@
+//charcodeat(): takes index and it returns char code(ASCII nmber) of the value at that index
+//string.charCodeAt(index)
+
+let string = "30 Days of java script"
+console.log(string.charAt(3))
+console.log(string.charCodeAt(3))
+let lastIndex = string.length -1
+console.log(string.charAt(lastIndex))
+console.log(string.charCodeAt(lastIndex))
