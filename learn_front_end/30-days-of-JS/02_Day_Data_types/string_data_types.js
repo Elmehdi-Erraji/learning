440dfbe0c3332ef919a77a928bb5b73d01ff5af1
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 01:27:12 +0100

    string data types

---
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/string_data_types.js
@@ -0,0 +1,9 @@
+let space = ''
+let firstname = 'mehdi'
+let lastname = 'raji'
+let country = 'morocco'
+let city ='agadir'
+let language ='JavaScript'
+let job = 'student'
+
+console.log(firstname,lastname,'lives in',country)
