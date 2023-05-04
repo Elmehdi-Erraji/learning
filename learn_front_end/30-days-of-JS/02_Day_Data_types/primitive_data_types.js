31ac89c2b8ff7c048d1a32fab223fc76e03ffc82
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 01:15:21 +0100

    primitive_data_types.js

---
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/primitive_data_types.js
@@ -0,0 +1,15 @@
+let word = 'JavaScript'
+// we don't modify string 
+// we don't do like this, word[0] = 'Y'
+
+let num1 = 3
+let num2 = 3
+console.log(num1 == num2)   // true
+
+let js = 'javascript'
+let py = 'python'
+console.log(js == py)       // false
+
+let lightOn = true
+let lightOff = false
+console.log(lightOn == lightOff) // false
