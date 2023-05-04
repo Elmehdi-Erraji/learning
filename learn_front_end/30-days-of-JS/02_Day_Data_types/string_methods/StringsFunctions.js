15ef82cda31458bec3d1533f9feac34d078a3c55
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 06:16:29 +0100

    string.concat() function

---
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/StringsFunctions.js
@@ -0,0 +1,8 @@
+//concat(): it takes many sbstrings and creates concatination
+//string.concat(sbstring, substring, substring)
+
+let string = '30'
+let space =' '
+console.log(string.concat(space,"days",space, "of",space, "js"))// 30 days of js
+let country = 'fin'
+console.log(country.concat("land")) // finlad
