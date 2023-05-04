c241ab8ccf3a16e4310beff06cbf99c8ad9de9a0
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 16:04:19 +0100

    string.endsWith() function

--- learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/StringsFunctions.js
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/StringsFunctions.js
@@ -1,8 +1,24 @@
 //concat(): it takes many sbstrings and creates concatination
 //string.concat(sbstring, substring, substring)
-
-let string = '30'
+/*
+let string0 = '30'
 let space =' '
 console.log(string.concat(space,"days",space, "of",space, "js"))// 30 days of js
 let country = 'fin'
 console.log(country.concat("land")) // finlad
+*/
+
+//endswitch :it takesa substring as an argument and it checks if the string ends with that spicific substring. it returns (true or false).
+//string.endswith(substring)
+
+let string1 = 'love is the best in this world'
+console.log(string1.endsWith('world'))
+console.log(string1.endsWith('love'))
+console.log(string1.endsWith('in this world'))
+
+let country = 'morocco'
+console.log(country.endsWith("o"))
+
+
+
+
