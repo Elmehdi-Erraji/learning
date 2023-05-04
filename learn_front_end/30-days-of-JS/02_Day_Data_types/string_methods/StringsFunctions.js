ed3632392803027d1412a5672e8621dd0e5f273e
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 16:29:13 +0100

    more string functions

--- learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/StringsFunctions.js
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/StringsFunctions.js
@@ -18,7 +18,7 @@ console.log(string1.endsWith('in this world'))
 
 let country = 'morocco'
 console.log(country.endsWith("o"))
-*/
+
 
 //includes(): it takes a substring argu and it check if substring argument exists in the string.
 //includes() returns a boolean (true or false )
@@ -28,10 +28,28 @@ console.log (string2.includes("banana"))
 console.log (string2.includes("mehdi"))
 
 
+*/
+// indexOf(substring): takes a substring and if the substring exists in a string it returns the first position of the substring if does not exist it returns -1
 
+let string3 ="i'm a computer sciense student"
+console.log(string3.indexOf('a'))
+console.log(string3.indexOf('student'))
+console.log(string3.indexOf('s'))
+console.log(string3.indexOf('x'))
 
+//lastIndexOf(): takesa substring and if the substring exist in a string it returns the last position of the substring if it does not exist it returns +1
 
+let string4 = 'I started learning java script to be a better front end developer'
+console.log(string4.lastIndexOf('started'))
+console.log(string4.lastIndexOf('better'))
+console.log(string4.lastIndexOf('front'))
 
+//lenght: the string lenght method returns the number of characters in a string includes empty. Example:
+
+let js ="JavaScript"
+console.log(js.length)
+
+//
 
 
 
