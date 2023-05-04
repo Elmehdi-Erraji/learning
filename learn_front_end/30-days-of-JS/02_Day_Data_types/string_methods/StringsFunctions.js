131cc95578e6543e89fe4aae914c845fd968797d
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 16:11:12 +0100

    string.includes() function

--- learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/StringsFunctions.js
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/StringsFunctions.js
@@ -6,7 +6,7 @@ let space =' '
 console.log(string.concat(space,"days",space, "of",space, "js"))// 30 days of js
 let country = 'fin'
 console.log(country.concat("land")) // finlad
-*/
+
 
 //endswitch :it takesa substring as an argument and it checks if the string ends with that spicific substring. it returns (true or false).
 //string.endswith(substring)
@@ -18,6 +18,30 @@ console.log(string1.endsWith('in this world'))
 
 let country = 'morocco'
 console.log(country.endsWith("o"))
+*/
+
+//includes(): it takes a substring argu and it check if substring argument exists in the string.
+//includes() returns a boolean (true or false )
+
+let string2 = "hi my name is mehdi and i'm 20 years old " 
+console.log (string2.includes("banana"))
+console.log (string2.includes("mehdi"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
