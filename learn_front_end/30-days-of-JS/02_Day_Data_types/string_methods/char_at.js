ecec51bf8e6a41bc1c00bc636f76b28b43742127
Mehdi <elmahdi20erraji@gmail.com>
2023-05-04 05:55:53 +0100

    char at methode

---
+++ learn_front_end/30-days-of-JS/02_Day_Data_types/string_methods/char_at.js
@@ -0,0 +1,6 @@
+// charAt(): takes index and it returns the value at the index
+let string = '30 days of java script'
+console.log(string.charAt(0))
+
+let lastIndex = string.length -1
+console.log(string.charAt(lastIndex))
